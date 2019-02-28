d70cf8a10aaa879b133f83fa7446e3d663d2e9eb
zhaohuanhuan <1297972106@qq.com>
2019-02-28 17:56:54 +0800

    2/28/5

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -29,7 +29,7 @@ pipeline {
             steps {
                 echo 'Deploying....'
                 sh 'npm install http-server -g'
-                sh 'cd ${dist} && http-server -p 8890 -o -P http://www.pipeline1.com'
+                sh 'cd ${dist} && http-server -p 8890'
             }
         }
     }
